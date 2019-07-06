48c5b39d4fb35d83ad9d6d2a65ad1f603d1ce2f2
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-07-06 18:43:52 +0100

    Refactor to do more in the fiber, get a more exact number of iterations for divergence

--- bench.rb
+++ bench.rb
@@ -2,38 +2,42 @@
 
 require "fiber"
 
-def mandel_fiber(c)
+def mandel_fiber(c, radius)
   Fiber.new do
     z = c
+    iter = 0
+    diverge = false
 
     # "If the series of Zs will always stay close to c and never trend away, that
     #  point is in the Mandelbrot set." -- Jonathan Coulton, "Mandelbrot Set"
     loop do
-      10.times { z = z * z + c }
-      Fiber.yield z
+      if z.abs > radius
+        diverge = true
+        Fiber.yield z, iter, diverge
+        break
+      end
+      iter += 1
+      z = z * z + c
+      Fiber.yield z, iter, diverge if iter % 10 == 0
     end
   end
 end
 
 10.times do
   test_point = Complex.rect(rand() * 2.0 - 1.0, rand() * 2.0 - 1.0)
-  diverge = false
 
-  f = mandel_fiber(test_point)
+  f = mandel_fiber(test_point, 2.0)
   puts "Point: #{test_point.inspect}"
 
-  8.times do |i|
-    z = f.resume
-    if z.abs > 2.0
-      diverge = true
-      puts "  -> Point diverges after #{i+1}0 iterations"
-      break
-    end
+  iter, diverge = 0, false
+
+  loop do
+    z, iter, diverge = f.resume
+    break if diverge
+    break if iter >= 80
   end
 
-  unless diverge
-    puts "  -> Point does not diverge after 80 iterations"
-  end
+  puts "  -> Point #{diverge ? "diverges" : "does not diverge"} after #{iter} iterations"
 
   puts "\n\n"
 end
