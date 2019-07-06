148af31ea40cda15a6e9c18389f1e32edc27b9f6
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-07-06 20:06:16 +0100

    Use known points

--- bench.rb
+++ bench.rb
@@ -2,6 +2,66 @@
 
 require "fiber"
 
+KNOWN_POINTS = [
+  {
+    r: 0.6834035815381081,
+    i: -0.3402545194889641,
+    iter: 2,
+    diverge: true,
+  },
+  {
+    r: 0.4574076276188268,
+    i: -0.3402545194889641,
+    iter: 11,
+    diverge: true,
+  },
+  {
+    r: -0.38336576121887034,
+    i: 0.31978497746348045,
+    iter: 80,
+    diverge: false,
+  },
+  {
+    r: -0.38311082319380874,
+    i: -0.640229531019499,
+    iter: 23,
+    diverge: true,
+  },
+  {
+    r: -0.5458855742667235,
+    i: 0.5192757454218333,
+    iter: 17,
+    diverge: true,
+  },
+  {
+    r: 0.01836587534857914,
+    i: -0.8817678930191915,
+    iter: 7,
+    diverge: true
+  },
+  {
+    r: -0.7516838256330622,
+    i: -0.10958951286014185,
+    iter: 27,
+    diverge: true
+  },
+  {
+    r: 0.3176599569166043,
+    i: 0.5939607508203428,
+    iter: 31,
+    diverge: true
+  }
+]
+
+#KNOWN_POINTS.concat ((1..10).map {
+#  {
+#    r: rand() * 2.0 - 1.0,
+#    i: rand() * 2.0 - 1.0,
+#    iter: 0,
+#    diverge: false,
+#  }
+#})
+
 def mandel_fiber(c, radius)
   Fiber.new do
     z = c
@@ -23,11 +83,12 @@ def mandel_fiber(c, radius)
   end
 end
 
-10.times do
-  test_point = Complex.rect(rand() * 2.0 - 1.0, rand() * 2.0 - 1.0)
+KNOWN_POINTS.each_with_index do |point, i|
+  STDERR.puts "Point: #{point.inspect}"
+  test_z = Complex.rect point[:r], point[:i]
 
-  f = mandel_fiber(test_point, 2.0)
-  puts "Point: #{test_point.inspect}"
+  f = mandel_fiber(test_z, 2.0)
+  puts "Point: #{test_z.inspect}"
 
   iter, diverge = 0, false
 
@@ -38,6 +99,10 @@ end
   end
 
   puts "  -> Point #{diverge ? "diverges" : "does not diverge"} after #{iter} iterations"
+
+  if [iter, diverge] != [point[:iter], point[:diverge]]
+    raise "Point #{i.inspect} fails! #{iter.inspect}, #{diverge.inspect} != (known) #{point[:iter].inspect}, #{point[:diverge].inspect}"
+  end
 
   puts "\n\n"
 end
